1568887323707ffec8f88bde59de498d45db97d6
Elliot-Haw <elliothaw@example.com>
2025-09-19 10:23:59 +1000

    Added basic guideline

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+/*This is the first set of comments just briefly organizing what should go on in this application
+
+1. Have a functioning UI
+2. Be split into components and functions each with their own tests based on what the best practice for Go is
+3. The user first of all enters the form which gets encrypted
+4. It gets passed to the AI which takes in the form
+5. The use then passes in the data which it should expect
+6. The AI will encrypt that
+7. Give multiple options when the user logs in. They can choose what form they are entering information for
+8. They upload the filled in form as a pdf.
+9. This again gets encrypted the same way
+10. The AI then reads this, processes the form and explains if any fields are missing, if any data is incorrect, or if it requires human review.
+11. The AI will be deepseek running locally
+12. Connect to postgres to store the data and the form types, train the AI based on the form encryption
+13. Use React for frontent, use Python for ML
+*/
+
 func main(){
 	fmt.Println("Hello, world")
 	fmt.Println("This is a second test")
